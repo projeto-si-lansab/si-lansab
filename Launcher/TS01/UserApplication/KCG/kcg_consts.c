879df253d38243c18bd048145f1665ff76791e1b
gpaduan <gpaduan8@gmail.com>
2014-10-23 02:31:27 -0200

    Uploading files needed by embedded compilation (KCG folder)

---
+++ Launcher/TS01/UserApplication/KCG/kcg_consts.c
@@ -0,0 +1,28 @@
+/* $*************** KCG Version 6.1.3 (build i6) ****************
+** Command: s2c613 -config C:/github/si-lansab/Launcher/TS01/UserApplication/KCG\kcg_s2c_config.txt
+** Generation date: 2014-10-23T02:21:18
+*************************************************************$ */
+
+#include "kcg_consts.h"
+
+/* PHASE_NOT_STARTED */
+const T_String PHASE_NOT_STARTED = { 'N', 'O', 'T', ' ', 'S', 'T', 'A', 'R',
+  'T', 'E', 'D', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+/* PHASE_IN_PROCESS */
+const T_String PHASE_IN_PROCESS = { 'I', 'N', ' ', 'P', 'R', 'O', 'G', 'R', 'E',
+  'S', 'S', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+/* PHASE_COMPLETE */
+const T_String PHASE_COMPLETE = { 'C', 'O', 'M', 'P', 'L', 'E', 'T', 'E', ' ',
+  ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+/* PHASE_FAILED */
+const T_String PHASE_FAILED = { 'F', 'A', 'I', 'L', 'E', 'D', ' ', ' ', ' ',
+  ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+/* $*************** KCG Version 6.1.3 (build i6) ****************
+** kcg_consts.c
+** Generation date: 2014-10-23T02:21:18
+*************************************************************$ */
+
