ea982d3e48ee6e36324f29f3416c432aeee89eba
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2014-11-04 20:41:36 -0200

    TS05 - rpi tests

--- Satellite/TS05/UserApplication/KCG/kcg_consts.c
+++ Satellite/TS05/UserApplication/KCG/kcg_consts.c
@@ -1,6 +1,6 @@
 /* $*************** KCG Version 6.1.3 (build i6) ****************
 ** Command: s2c613 -config C:/GitHub/si-lansab/Satellite/TS05/UserApplication/KCG\kcg_s2c_config.txt
-** Generation date: 2014-11-04T20:00:01
+** Generation date: 2014-11-04T20:38:53
 *************************************************************$ */
 
 #include "kcg_consts.h"
@@ -47,6 +47,6 @@ const String_ES CDS_TEXT_AUTOVERIFY_CORRECT = { 'A', 'U', 'T', 'O', ' ', 'V',
 
 /* $*************** KCG Version 6.1.3 (build i6) ****************
 ** kcg_consts.c
-** Generation date: 2014-11-04T20:00:01
+** Generation date: 2014-11-04T20:38:53
 *************************************************************$ */
 
--- Satellite/TS05IntegrationTest/UserApplication/KCG/ts05.c
+++ Satellite/TS05IntegrationTest/UserApplication/KCG/ts05.c
@@ -1,6 +1,6 @@
 /* $*************** KCG Version 6.1.3 (build i6) ****************
 ** Command: s2c613 -config C:/GitHub/si-lansab/Satellite/TS05IntegrationTest/UserApplication/KCG\kcg_s2c_config.txt
-** Generation date: 2014-11-04T20:01:20
+** Generation date: 2014-11-04T20:40:08
 *************************************************************$ */
 
 #include "kcg_consts.h"
@@ -614,6 +614,6 @@ void ts05(inC_ts05 *inC, outC_ts05 *outC)
 
 /* $*************** KCG Version 6.1.3 (build i6) ****************
 ** ts05.c
-** Generation date: 2014-11-04T20:01:20
+** Generation date: 2014-11-04T20:40:08
 *************************************************************$ */
 
