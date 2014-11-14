7c1299a8ccc61321a7acfbbd57c81a5d21065036
gpaduan <gpaduan8@gmail.com>
2014-11-14 00:42:30 -0200

    Moved ARDrone folder from TS01 code to si-lansab top level.
    Fixed TS01 "DF ratio" in order to comply with other teams.

--- ARDrone/arDemo.py
+++ ARDrone/arDemo.py
@@ -17,19 +17,19 @@ import json
 SRV_ADDR = "/tmp/socket"
 
 PRE_LAUNCH = 'PRE LAUNCH'
-PHASE_CHANGE = 'PHASE CHANGE'
-PHASE_1 = 'PHASE 1'
-PHASE_2 = 'PHASE 2'
-PHASE_3 = 'PHASE 3'
-PHASE_4 = 'PHASE 4'
-PHASES = [PHASE_1, PHASE_2, PHASE_3, PHASE_4]
+STAGE_CHANGE = 'STAGE CHANGE'
+STAGE_1 = 'STAGE 1'
+STAGE_2 = 'STAGE 2'
+STAGE_3 = 'STAGE 3'
+STAGE_4 = 'STAGE 4'
+STAGES = [STAGE_1, STAGE_2, STAGE_3, STAGE_4]
 
 scade_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
 # -------------------------------------------------------------------------------------------------
 # main
 # -------------------------------------------------------------------------------------------------
-def main ():
+def main():
 
     # Make sure the socket does not already exist
     try:
@@ -42,19 +42,19 @@ def main ():
     scade_socket.bind(SRV_ADDR)
 
     # Listen for incoming connections
-    scade_socket.listen(1)
+    scade_socket.listen(5)
 
-    # drone = libARDrone.ARDrone ()
-    # drone.reset ()
+    # drone = libARDrone.ARDrone()
+    # drone.reset()
     drone_status = PRE_LAUNCH
-    drone_phase = 0
+    drone_stage = 0
 
-    connection, client_address = scade_socket.accept()
-    print >>sys.stderr, 'connection from', client_address
     running = True
 
     while running:
         try:
+            connection, client_address = scade_socket.accept()
+            print >>sys.stderr, 'connection from', client_address
             data = connection.recv(256)
             print >>sys.stderr, 'received "%s"' % data
             if data:
@@ -65,28 +65,27 @@ def main ():
 
                 if rocket_destroy:
                     print '--- Rocket Destroyed ---'
-                    # drone.halt()
+                    # drone.land()
                 elif rocket_launch and drone_status == PRE_LAUNCH:
                     print '--- Lauching ARDrone ---'
-                    drone_status = PHASE_1
-                    drone_phase = 1
+                    drone_status = STAGE_1
+                    drone_stage = 1
                     # drone.takeoff()
-                elif drone_status in PHASES and change_state and drone_status != PHASE_CHANGE:
-                    print '--- Changing Phase ---'
-                    drone_status = PHASE_CHANGE
+                elif drone_status in STAGES and change_state and drone_status != STAGE_CHANGE:
+                    print '--- Changing Stage ---'
+                    drone_status = STAGE_CHANGE
                     # drone.event_turnaround()
-                elif drone_status == PHASE_CHANGE and not change_state:
-                    print '--- Entering Phase ---'
-                    drone_phase += 1
+                elif drone_status == STAGE_CHANGE and not change_state:
+                    print '--- Entering Stage ---'
+                    drone_stage += 1
                     try:
-                        drone_status = PHASES[drone_phase]
+                        drone_status = STAGES[drone_stage]
                     except IndexError:
-                        print "--- Invalid Phase Change. Shutting Down.---",
+                        print "--- Invalid Stage Change. Shutting Down.---",
                         running = False
-                elif drone_phase in PHASES:
+                elif drone_stage in STAGES:
                     # drone.move_up()
                     pass
-
             else:
                 print >>sys.stderr, 'no more data from', client_address
         finally:
@@ -97,7 +96,8 @@ def main ():
         #print "bat: ", bat
 
     print "Shutting down...",
-    #drone.halt ()
+    # drone.land()
+    # drone.halt ()
     print "Ok."
 
 # -------------------------------------------------------------------------------------------------
@@ -105,6 +105,6 @@ def main ():
 # -------------------------------------------------------------------------------------------------
 if ( "__main__" == __name__ ):
 
-    main ()
+    main()
 
 # < THE END >-------------------------------------------------------------------------------------- #
