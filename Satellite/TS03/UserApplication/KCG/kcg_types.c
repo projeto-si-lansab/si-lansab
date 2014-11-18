c5a7b46d0afd2613a131e6e68c765112f02e54e4
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2014-11-18 21:21:10 -0200

    TS03 - Uploading KCG files for raspberry pi

---
+++ Satellite/TS03/UserApplication/KCG/kcg_types.c
@@ -0,0 +1,12 @@
+/* $*************** KCG Version 6.1.3 (build i6) ****************
+** Command: s2c613 -config C:/GitHub/si-lansab/Satellite/TS03/UserApplication/KCG\kcg_s2c_config.txt
+** Generation date: 2014-11-18T21:20:02
+*************************************************************$ */
+
+#include "kcg_types.h"
+
+/* $*************** KCG Version 6.1.3 (build i6) ****************
+** kcg_types.c
+** Generation date: 2014-11-18T21:20:02
+*************************************************************$ */
+
